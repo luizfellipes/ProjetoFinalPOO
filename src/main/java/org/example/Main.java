abf89c95eff36ec2df069555ac4496cbde497836
Luiz Fellipe <luiz.13.silva@gmail.com>
2023-01-29 14:12:32 -0400

    ProjetoFinal POO Parte 2, Implementado a classe visualização com agregação das classes gafanhoto e video, finalizando o projeto

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -8,14 +10,24 @@ public class Main {
         video[0] = new Video("Aula 1 de POO");
         video[1] = new Video("Aula 12 de Java");
         video[2] = new Video("Aula 10 de HTML5");
-        System.out.println(video[0]);
-
 
 
         Gafanhoto[] gafanhoto = new Gafanhoto[2];
 
         gafanhoto[0] = new Gafanhoto("Jubileu", 22, "M", "juba");
         gafanhoto[1] = new Gafanhoto("Creuza", 12, "F", "creuzita");
-        System.out.println(gafanhoto[0]);
+
+
+        Visualizacao[] visualizacao = new Visualizacao[5];
+
+        visualizacao[0] = new Visualizacao(gafanhoto[0], video[1]);
+        visualizacao[0].avaliar();
+        visualizacao[1] = new Visualizacao(gafanhoto[0], video[1]);
+        visualizacao[0].avaliar(87.0f);
+        visualizacao[2] = new Visualizacao(gafanhoto[0], video[1]);
+        visualizacao[0].avaliar(8);
+        System.out.println(visualizacao[0]);
+
+
     }
 }
